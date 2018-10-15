e6f66628eb9a9469dc9f66dbbefcae7625218d3a
hbrooker97 <42081142+hbrooker97@users.noreply.github.com>
2018-10-15 18:09:00 +0800

    fix question script

--- js/script.js
+++ js/script.js
@@ -255,7 +255,14 @@ function showInstructionsQual() {
 
 
 //QUESTION BOX
-var questions = ["What is agonism? <br><br><img src=\"images/agonistchart.png\" alt=\"Ago1 Answer\" style=\"max-height:100%; max-width:100%;\">",
+var questionsAgonist = ["What is agonism?",
+    "Does increasing agonist affinity shift the agonist dose-response curve to the RIGHT or the LEFT?<br><i>This can be tested using the Visualiser</i>",
+    "Does increasing agonist affinity INCREASE or DECREASE the EC<sub>50</sub> value of the agonist?<br><i>This can be tested using the Visualiser</i>",
+    "Is AFFINITY required for an agonist to induce an effect?<br><i>This can be tested using the Visualiser</i>",
+    "Is INTRINSIC EFFICACY required for an agonist to induce an effect? This can be tested using the Visualiser<br><i>This can be tested using the Visualiser</i>",
+    "Are RECEPTORS required for an agonist to induce an effect? This can be tested using the Visualiser<br><i>This can be tested using the Visualiser</i>",
+    "Is COUPLING EFFICIENCY required for an agonist to induce an effect?<br><i>This can be tested using the Visualiser</i>",
+    "What is agonist POTENCY?",
     "What is a PARTIAL agonist?",
     "What is a FULL agonist?",
     "For a partial agonist, increasing which of the following variables will increase the maximum effect produced – intrinsic efficacy (ε), receptor density (R<sub>T</sub>) or coupling efficiency (<i>f</i>)? <br><i>This can be tested using the Visualiser</i>",
@@ -263,7 +270,14 @@ var questions = ["What is agonism? <br><br><img src=\"images/agonistchart.png\" 
     "Can a full agonist produce a maximum response when occupying and activating only a fraction of the total receptor population?<br><i>This can be tested using the Visualiser</i>",
     "Can an agonist be a partial agonist in one cell type, be a full agonist in a different cell type?"];
 				
-var answers = ["It is represented by the red ball on the agonist dose-response curve.  The pD<sub>2</sub> is another measure of agonist potency, and is simply the –logEC<sub>50</sub>.",
+var answersAgonist = ["<br><br><img src=\"images/agonistchart.png\" alt=\"Ago1 Answer\" style=\"max-height:100%; max-width:100%;\">",
+    "Increasing agonist affinity will shift the agonist dose-response curve to the <b>LEFT</b>.  Increasing affinity increases the number of receptors that are bound and activated by the agonist. Increasing affinity is associated with a reduction in the K<sub>A</sub> value, as a lower [agonist] will be required to occupy 50% of receptors. This effect can be observed using the visualiser.",
+    "The EC<sub>50</sub> is the [agonist] that produces 50% of the maximum effect produced by that agonist.  Increasing agonist affinity will <b>DECREASE</b> the EC<sub>50</sub> value because agonists with higher affinity require a <b>LOWER</b> [agonist] to achieve receptor binding and activation, and to produce an effect. This effect can be observed using the visualiser.",
+    "<b>YES</b>, at least some level of affinity is required for an agonist to bind to receptors and induce an effect – when agonist affinity = 0, agonist-induced effect = 0.  This effect can be observed using the visualiser.",
+    "<b>YES</b>, at least some level of Intrinsic Efficacy is  required to turn the binding signal into receptor activation, and thus produce an effect – when ε = 0, agonist-induced effect = 0.  This effect can be observed using the visualiser. ",
+    "<b>YES</b>, an agonist cannot induce an effect if the cell contains no functional receptors for the agonist – when R<sub>T</sub> = 0, agonist-induced effect = 0. This effect can be observed using the visualiser.",
+    "<b>YES</b>, an agonist cannot induce an effect if the cell contains no functioning signalling pathways that link the activated receptor to the observed effect – when <i>f</i> = 0, agonist-induced effect = 0.  This effect can be observed using the visualiser.",
+    "Potency is one expression of the activity of a drug, in terms of the concentration or amount needed to produce a defined effect.  Potency depends on both agonist-dependent (affinity, efficacy) and cell-dependent (R<sub>T</sub>, <i>f</i> ) parameters, and thus can vary significantly between different cells and tissues. The term DOES NOT refer to the maximum effect attainable.  The EC<sub>50</sub> value is a commonly used measure of agonist potency and is the molar concentration of the agonist that produces 50% of the maximum response induced by that agonist (NOT the [agonist] producing 50% max. effect). It is represented by the red ball on the agonist dose-response curve.  The pD<sub></sub> is another measure of agonist potency, and is simply the –logEC<sub>50</sub>.",
     "An agonist that in a given tissue, under specified conditions, cannot elicit as large an effect as can another agonist acting through the same receptors in the same cell or tissue (even when applied at high concentration, so that all the receptors should be occupied).  The default agonist shown is a partial agonist.",
     "An agonist that in a given tissue, under specified conditions, elicits the largest possible effect (100%Emax).  Increase ε, R<sub>T</sub> and/or <i>f</i>  to change a partial agonist into a full agonist.",
     "For a partial agonist, increasing any of the following variables – intrinsic efficacy (ε), receptor density (R<sub>T</sub>) or coupling efficiency (<i>f</i>) – will increase the maximum effect produced.  Increasing these variables may change a partial agonist into a full agonist. This effect can be observed using the visualiser.",
@@ -271,40 +285,40 @@ var answers = ["It is represented by the red ball on the agonist dose-response c
     "<b>YES</b>, a full agonist can produce a maximum response when occupying only a fraction of the total receptor population (less than 100%).  That is, not all of the receptors in the tissue are required to achieve a maximal response with some high efficacy agonists. This has been amply demonstrated experimentally in that reducing the number of functional receptors (e.g. by using irreversible antagonist) may result in a decrease in agonist potency without a decreased maximal response. At sufficiently high degrees of receptor inactivation, the maximum response even to full agonists is finally reduced.  This is often referred to as the cell having ‘spare receptors’ or a ‘receptor reserve’ (not all receptors are required to induce a maximal response). This effect can be observed using the visualiser.",
     "<b>YES</b>.  Agonists with low-moderate efficacy may act as full agonists in cells with high R<sub>T</sub> and/or <i>f</i>, but act as partial agonists in cells with low R<sub>T</sub> and/or <i>f</i>.  Test the effects of increasing or decreasing R<sub>T</sub> and/or <i>f</i> on agonist-induced effects. This effect can be observed using the visualiser."];
 
-var questionCounter = 0;
-document.getElementById("agonistQuestion").innerHTML = "<b>" + questions[questionCounter] + "</b>";
+var questionCounterAgonist = 0;
+document.getElementById("agonistQuestion").innerHTML = "<b>" + questionsAgonist[questionCounterAgonist] + "</b>";
 
 
 function revealAnswerAgonist(){
-	document.getElementById("agonistAnswer").innerHTML = answers[questionCounter];
+    document.getElementById("agonistAnswer").innerHTML = answersAgonist[questionCounterAgonist];
     $('#agonistAnswerModal').modal('show');
 }
 
 
 function nextQuestionAgonist() {
-    if (questionCounter + 1 == questions.length) { //end of questions
+    if (questionCounterAgonist + 1 == questionsAgonist.length) { //end of questions
         document.getElementById("agonistQuestion").style.display = "none";
         document.getElementById("revealAgonistAnswer").style.display = "none";
-        document.getElementById("restartMessage").style.display = "block";
-        document.getElementById("restartQuestion").style.display = "block";
+        document.getElementById("restartMessageAgonist").style.display = "block";
+        document.getElementById("restartQuestionAgonist").style.display = "block";
         document.getElementById("nextAgonistQuestion").style.display = "none";
-        document.getElementById("agonistQuestion").innerHTML = "<b>" + questions[questionCounter] + "</b>";
-        questionCounter = 0;
+        document.getElementById("agonistQuestion").innerHTML = "<b>" + questionsAgonist[questionCounterAgonist] + "</b>";
+        questionCounterAgonist = 0;
     }
     else {
-        questionCounter++; 
-        document.getElementById("restartMessage").style.display = "none";
-        document.getElementById("restartQuestion").style.display = "none";
-        document.getElementById("agonistQuestion").innerHTML = "<b>" + questions[questionCounter] + "</b>";
+        questionCounterAgonist++; 
+        document.getElementById("restartMessageAgonist").style.display = "none";
+        document.getElementById("restartQuestionAgonist").style.display = "none";
+        document.getElementById("agonistQuestion").innerHTML = "<b>" + questionsAgonist[questionCounterAgonist] + "</b>";
     }
 }
 
 function restartQuestionAgonist() {
     document.getElementById("agonistQuestion").style.display = "block";
     document.getElementById("nextAgonistQuestion").style.display = "block";
-    document.getElementById("restartMessage").style.display = "none";
-	document.getElementById("restartQuestion").style.display = "none";
-	document.getElementById("agonistQuestion").innerHTML = "<b>" + questions[questionCounter] + "</b>";
+    document.getElementById("restartMessageAgonist").style.display = "none";
+    document.getElementById("restartQuestionAgonist").style.display = "none";
+    document.getElementById("agonistQuestion").innerHTML = "<b>" + questionsAgonist[questionCounterAgonist] + "</b>";
 	document.getElementById("revealAgonistAnswer").style.display = "block";
 }
 
